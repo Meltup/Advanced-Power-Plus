242c5037b4cdd84bd8f36c430534a78f5f9948fb
Leugimimi <leugimimi@gmail.com>
2022-10-22 16:52:06 -0300

    1.4
    The update to 1.4

--- Sources/MyClass.cs
+++ Sources/MyClass.cs
@@ -17,39 +17,57 @@ namespace sd_adv_powergen
     [StaticConstructorOnStartup]
     public class sd_adv_powergen_CompAdvPowerPlantSolar : CompPowerPlantSolar
     {
-        private static readonly Vector2 sd_adv_powergen_BarSize = new Vector2(2.3f, 0.14f);
-
-        private static readonly Material BarFilledMat = SolidColorMaterials.SimpleSolidColorMaterial(new Color(0.5f, 0.475f, 0.1f));
-
-        private static readonly Material BarUnfilledMat = SolidColorMaterials.SimpleSolidColorMaterial(new Color(0.15f, 0.15f, 0.15f));
-
-        private float MinPowerOutput => 0f;
-        private float MaxPowerOutput => -this.Props.basePowerConsumption;
-
         protected override float DesiredPowerOutput
         {
             get
             {
-                return Mathf.Lerp(this.MinPowerOutput, this.MaxPowerOutput, this.parent.Map.skyManager.CurSkyGlow) * this.RoofedPowerOutputFactor;
+                return Mathf.Lerp(0f, FullSunPower, this.parent.Map.skyManager.CurSkyGlow) * this.RoofedPowerOutputFactor;
             }
         }
 
-        private float RoofedPowerOutputFactor => this.parent.OccupiedRect().Average(cell => this.parent.Map.roofGrid.Roofed(cell) ? 0 : 1f);
+        private float RoofedPowerOutputFactor
+        {
+            get
+            {
+                int num = 0;
+                int num2 = 0;
+                foreach (IntVec3 c in this.parent.OccupiedRect())
+                {
+                    num++;
+                    if (this.parent.Map.roofGrid.Roofed(c))
+                    {
+                        num2++;
+                    }
+                }
+                return (float)(num - num2) / (float)num;
+            }
+        }
 
         public override void PostDraw()
         {
-            GenDraw.FillableBarRequest r = default;
+            base.PostDraw();
+            GenDraw.FillableBarRequest r = default(GenDraw.FillableBarRequest);
             r.center = this.parent.DrawPos + Vector3.up * 0.1f;
-            r.size = sd_adv_powergen_BarSize;
-            r.fillPercent = this.PowerOutput / this.MaxPowerOutput;
-            r.filledMat = BarFilledMat;
-            r.unfilledMat = BarUnfilledMat;
+            r.size = BarSize;
+            r.fillPercent = base.PowerOutput / FullSunPower;
+            r.filledMat = PowerPlantSolarBarFilledMat;
+            r.unfilledMat = PowerPlantSolarBarUnfilledMat;
             r.margin = 0.15f;
             Rot4 rotation = this.parent.Rotation;
             rotation.Rotate(RotationDirection.Clockwise);
             r.rotation = rotation;
             GenDraw.DrawFillableBar(r);
         }
+
+        private const float FullSunPower = 3400f;
+
+        private const float NightPower = 0f;
+
+        private static readonly Vector2 BarSize = new Vector2(2.3f, 0.14f);
+
+        private static readonly Material PowerPlantSolarBarFilledMat = SolidColorMaterials.SimpleSolidColorMaterial(new Color(0.5f, 0.475f, 0.1f), false);
+
+        private static readonly Material PowerPlantSolarBarUnfilledMat = SolidColorMaterials.SimpleSolidColorMaterial(new Color(0.15f, 0.15f, 0.15f), false);
     }
 
     [DefOf]
